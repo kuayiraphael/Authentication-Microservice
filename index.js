8797b5f2e175f956290c2becb6cc36440d39af51
Kuayi Raphael <tinacity111@gmail.com>
2025-01-03 18:51:42 +0000

    vercel

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const { CreateChannel, SubscribeMessage } = require("./utils");
 const customerRoutes = require("./api/customer");
+const appEvents = require("./api/app-events");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const redis = require('redis');
+const redis = require("redis");
 const print = console.log;
 
 dotenv.config(); // Load environment variables from .env file
 
 // Create Redis client(~)
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
 });
 
-redisClient.on('error', (err) => {
-  console.log('Redis Client Error', err);
+redisClient.on("error", (err) => {
+  console.log("Redis Client Error", err);
 });
 
-redisClient.on('connect', () => {
-  console.log('Redis client connected');
+redisClient.on("connect", () => {
+  console.log("Redis client connected");
 });
 
 const app = express();
@@ -32,20 +33,24 @@ async function startApp() {
     await mongoose.connect(process.env.DB_URI);
     print("Auth Service Database Connected");
 
-    await redisClient.connect();  // Connect to Redis(~)
+    await redisClient.connect(); // Connect to Redis(~)
 
     const channel = await CreateChannel();
 
-   // await customerRoutes(app, channel);
+    // await customerRoutes(app, channel);
 
-    await customerRoutes(app, channel, redisClient);  // Pass Redis client to routes(~)
+    await customerRoutes(app, channel, redisClient);
 
-    app.listen(8001, () => {
-      console.log("Customer is Listening to Port 8001");
-    });
+    app.get("/", (req, res) => {
+      res.send("Customer Service Running");
+    }); // Pass Redis client to routes(~)
+
+    // app.listen(8001, () => {
+    //   console.log("Customer is Listening to Port 8001");
+    // });
   } catch (err) {
     console.log("Failed to start app:", err);
   }
 }
 
-module.exports = startApp;
+module.export= startApp();
